e0deb7e9f2dd06bf7d51763fbe65678ff7789f13
asharath6 <asharath6@gmail.com>
2017-04-03 10:34:05 +0530

    small

--- modules/apache/manifests/install.pp
+++ modules/apache/manifests/install.pp
@@ -5,7 +5,7 @@ class apache::install {
     # resources
     $pack_name = $facts['os']['family']?{
         'Redhat' => 'httpd'
-        'Debain' => 'apache2'
+        'Debian' => 'apache2'
     }
     package { $pack_name:
         ensure => installed,
