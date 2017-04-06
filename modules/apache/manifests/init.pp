31236b0c0830e38aa4bbf88f75b661f3a77cf921
root <root@ip-172-31-13-119.us-west-2.compute.internal>
2017-04-06 06:48:30 +0000

    change is done

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -43,9 +43,6 @@
 # Copyright 2017 Your name here, unless otherwise noted.
 #
 class apache {
-include apache::install
-include apache::homepage
-include apache::service
-#include apache::uninstall
+
 
 }
